2c6740fd195d2f7201b7cf0fc0ba887cdb824378
letumar <84676632+letumar@users.noreply.github.com>
2023-09-11 09:52:28 +0200

    Update GreetingController.java

--- src/main/java/com/showcase/codewithspring/GreetingController.java
+++ src/main/java/com/showcase/codewithspring/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
     public List<String> hello() {
         List<String> list = new ArrayList<>();
         for (int i=0; i < 1; i++) {
-            list.add("Spring is here 2!");
+            list.add("Spring is here!");
         }
         return list;
     }
